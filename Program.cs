7eab2ce4d3974878f43236b8422290b8b5ec64b3
MuhammadWildanA <124054151+MuhammadWildanA@users.noreply.github.com>
2023-02-02 11:18:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using v21;
+
+Console.Write("Masukkan panjangnya : ");
+int p = Convert.ToInt32(Console.ReadLine());
+Console.Write("Masukkan lebarnya : ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+Rumus rumus = new Rumus();
+int luas = rumus.persegiPanjang(p,l);
+
+Console.WriteLine($"persegi panjang dengan panjang = {p} dan lebar = {l} maka luasnya adalah {luas}");
---
+++ v21.cs
@@ -0,0 +1,29 @@
+namespace v21 
+{
+
+
+    class Kalkulator 
+    {
+        public void tambah(){
+            Console.WriteLine("Tambah");
+        }
+
+        public void kurang(){
+            Console.WriteLine("Kurang");
+        }
+    }
+
+
+    class Rumus 
+    {
+         public void lingkaran(){
+            Console.WriteLine("lingkaran");
+        }
+
+         public int persegiPanjang(int p, int l){
+            return p*l;
+        }
+    }
+
+
+}
